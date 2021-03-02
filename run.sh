79a6675a3ab92f59f749826e2ff32ae585e7333c
alok676875 <74966945+alok676875@users.noreply.github.com>
2021-03-02 14:15:03 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,16 @@
 #! /bin/bash
+
 printf "Setting Up Remote Server... " >&2
 {
+sudo useradd -m ALOK
+sudo adduser ALOK sudo
+echo 'ALOK:8426' | sudo chpasswd
+sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
-sudo apt install nautilus nano -y 
 sudo adduser ALOK chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Complete " >&2 ||
